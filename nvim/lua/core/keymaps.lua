12314fd712cf9a86ac4d97bd23fb02230b9b2bcd
Patricia Severance <patriciakseverance@gmail.com>
2023-07-02 17:28:01 -0600

    Initial commit

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = ' '
+
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.wo.number = true
+
---
+++ nvim/lua/core/plugin_config/init.lua
@@ -0,0 +1,5 @@
+require('core.plugin_config.rose-pine')
+require('core.plugin_config.nvim-tree')
+require('core.plugin_config.bufferline')
+require('core.plugin_config.telescope')
+require('core.plugin_config.treesitter')
