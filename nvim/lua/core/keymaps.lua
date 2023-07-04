802f0000efc2fb44aa04c5c424095f3849500d38
Patricia Severance <patriciakseverance@gmail.com>
2023-07-03 20:20:13 -0600

    Add gitsigns toggle_current_line_blame keymap to keymaps.lua

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,3 +5,5 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 vim.wo.number = true
 
+vim.keymap.set('n', '<leader>t', ':Gitsigns toggle_current_line_blame<CR>')
+vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
