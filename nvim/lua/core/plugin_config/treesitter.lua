184cd7dc20c5f2427a6d7af3e63ddf6ec67897e5
Patricia Severance <78667757+pkseverance@users.noreply.github.com>
2024-09-13 00:07:43 -0600

    cleanup

--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or 'all'
-  ensure_installed = {'c', 'lua', 'python', 'ruby', 'rust', 'typescript', 'tsx', 'vue', 'javascript', 'css', 'json', 'yaml', 'toml', 'markdown', 'markdown_inline', 'bash'},
+  ensure_installed = {'lua', 'python', 'rust', 'go', 'typescript', 'tsx', 'vue', 'javascript', 'css', 'json', 'yaml', 'toml', 'markdown', 'markdown_inline', 'bash'},
 
   -- Install parsers synchronously (only applied to 'ensure_installed')
   sync_install = false,
